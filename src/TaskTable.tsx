625cfffb716b416c822a1151af32320658623310
ktama <ktama@users.noreply.github.com>
2022-06-26 23:41:32 +0900

    add: TaskTable

---
+++ src/TaskTable.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function TaskTable() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    header test
+                </p>
+            </header>
+            <body className="App-body">
+                <p>
+                    body test
+                </p>
+            </body>
+        </div>
+    )
+}
+
+export default TaskTable
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import TaskTable from './TaskTable'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <TaskTable />
   </React.StrictMode>
 )
