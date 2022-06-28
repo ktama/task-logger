b3d9febdf11360255e137728fa24b7572ac50b39
ktama <ktama@users.noreply.github.com>
2022-06-29 00:47:34 +0900

    fix: 不要な変数を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { listen } from '@tauri-apps/api/event'
 import dayjs from 'dayjs'
 
 function App() {
-  const [count, setCount] = useState(0);
   const [nowStr, getNow] = useState(dayjs().format('YYYY-MM-DD HH:mm:ss'));
 
   useEffect(() => {
@@ -25,7 +23,6 @@ function App() {
     }
   }, [])
 
-
   return (
     <div className="App">
       <header className="App-header">
@@ -33,28 +30,6 @@ function App() {
       </header>
       <body className='App-body'>
         <p>Body</p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </body>
     </div>
   )
